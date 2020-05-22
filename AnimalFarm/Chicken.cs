00b2907cadb0bb0ab8fd8cf1814b436e3122dc29
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-22 12:10:05 +0200

    Add project files.

---
+++ AnimalFarm/Chicken.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    class Chicken
+    {
+        
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (NotNullNotEmptuNotWhiteSpace(name))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new System.ArgumentException("name not valid");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+
+                if (value < 0 || value > 15)
+                {
+                    throw new System.ArgumentException("age should be between 0 and 15");
+                }
+                this.age = value;
+            }
+        }
+
+        public int ProductPerDay { 
+            get { return CalculateProductPerDay(age); }}
+
+        private bool NotNullNotEmptuNotWhiteSpace(string toValidate)
+        {
+            return string.IsNullOrEmpty(toValidate) & string.IsNullOrWhiteSpace(toValidate);
+        }
+
+        private int CalculateProductPerDay(int chickenAge)
+        {
+            if (chickenAge < 1)
+            {
+                Console.WriteLine("chicken doesn't produce any ages at this age");
+                return 0;
+            } else if(chickenAge>1 && chickenAge<10)
+            {
+                return 3;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+
+    }
+}
---
+++ AnimalFarm/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the chicken name and age, type 1 to exit the program");
+
+            var cmdArgs = Console.ReadLine().Split();
+            Chicken chicken = new Chicken(cmdArgs[0], int.Parse(cmdArgs[1]));
+            Console.WriteLine($"Chicekn {chicken.Name} age of {chicken.Age} can produce {chicken.ProductPerDay}.");
+
+            Console.ReadLine();
+        }
+    }
+}
